657105da25403a3e8d2021734009bdb892b533ce
SnusP <90616689+SnusP@users.noreply.github.com>
2024-11-01 16:05:17 +0300

    Delete back directory

--- back/utils/checkPassword.ts
+++
@@ -1,6 +0,0 @@
-import bcrypt from "bcrypt"
-import { UserAttributes } from "../models/users/User"
-
-export default (user: UserAttributes, password: string) => {
-    return bcrypt.compareSync(password, user.password)
-}
